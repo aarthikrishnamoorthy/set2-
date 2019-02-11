ba3a7c3fba75e96037f26ef4033d914c67cf7790
aarthikrishnamoorthy <47214239+aarthikrishnamoorthy@users.noreply.github.com>
2019-02-11 13:58:19 +0530

    amstrong btwn interval.py

---
+++ amstrong btwn interval.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+for i in range(n,k+1):
+    summ=0
+    t=i
+    while t>0:
+        digit=t%10
+        summ+=digit**3
+        t=t//10
+    if i==summ:
+        print(i)
