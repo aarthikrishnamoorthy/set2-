53982044d70560b3503ad2f84c4f53b089157ef5
aarthikrishnamoorthy <47214239+aarthikrishnamoorthy@users.noreply.github.com>
2019-02-12 13:49:24 +0530

    Update prime no btwn interval.py

--- prime no btwn interval.py
+++ prime no btwn interval.py
@@ -1,8 +1,8 @@
-n,k=map(int,input().split())
-for i in range(n,k+1):
+a,b=map(int,input().split())
+for i in range(a,b+1):
     if i>1:
         for j in range(2,i):
             if i%j==0:
                 break
         else:
-            print(i)
+               print(i)
